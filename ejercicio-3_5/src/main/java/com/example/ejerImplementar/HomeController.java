9739ab67b08678edb48253c5b30217fb79289497
Amanda PV <amandaperezvazquez1@gmail.com>
2025-10-12 01:17:59 +0200

    change name controller

--- ejercicio-3_5/src/main/java/com/example/ejerImplementar/HomeController.java
+++ ejercicio-3_5/src/main/java/com/example/ejerImplementar/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
     public String showHome( 
 
 
-            @RequestParam(required = false, defaultValue = "X") String userName, Model model) {
+            @RequestParam(required = false, defaultValue = "") String userName, Model model) {
             
             LocalDate fecha = LocalDate.now();
             
