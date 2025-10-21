6424fc61c5225b1b7005829f68b1822ee5bae2ce
Amanda Perez <amandaperezvazquez1@gmail.com>
2025-10-21 14:27:29 +0200

    new exercise 5-1

---
+++ tema_5/ejercicio-5_1/src/test/java/com/example/simple_form/MainTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
